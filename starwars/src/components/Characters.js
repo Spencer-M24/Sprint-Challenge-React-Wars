8fd8ce2149e2fa5ad8edd61f4fef777d650ae890
Spencer Meredith <Smeredith1119@gmail.com>
2019-02-15 08:35:35 -0800

    added characters component, imports and exports from top to bottom props

---
+++ starwars/src/components/Characters.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Character from "./Character";
+
+
+const Characters = () => {
+    return
+    <div className="characters">{props.characters.map( character =>
+        <Character character {Character} />)}
+    
+    </div> 
+
+    )}
+
+
+    export default Characters;
