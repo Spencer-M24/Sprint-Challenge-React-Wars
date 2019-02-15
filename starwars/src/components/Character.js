edc639953361fada905c483422902cf53e3301e4
Spencer Meredith <Smeredith1119@gmail.com>
2019-02-15 09:27:19 -0800

    fixed props on eyes not showing up

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -8,7 +8,7 @@ const Character = (props) => {
         <div>Name: {props.character.name}</div>
         <div>Gender: {props.character.gender}</div>
         <div>Hair Color: {props.character.hair_color}</div>
-        <div>Eyes: {props.character.eyes}</div>
+        <div>Eyes: {props.character.eye_color}</div>
         <div>Height: {props.character.height}</div>
         <div>Mass: {props.character.mass}</div>
 
